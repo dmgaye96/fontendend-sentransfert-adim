56db997e63386f9fc6d9f02d0fbd7410b0d43979
dmgaye <doudoumohametgaye@gmail.com>
2019-09-11 05:48:19 +0000

    teste jspdf ok

--- src/app/adduser/adduser.component.ts
+++ src/app/adduser/adduser.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import * as jsPDF from 'jspdf';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-adduser',
   templateUrl: './adduser.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdduserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router  ) { }
 
   ngOnInit() {
   }
 
+@ViewChild('content', {static: false}) content:ElementRef;
+/*   @ViewChild ('content') content: ElementRef;
+ */
+public downloadPDF(){
+  let doc=new jsPDF();
+
+  let specialElementHandlers={
+    '#editor': function(element, renderer){
+      return true;
+    }
+  };
+
+  let content=this.content.nativeElement;
+
+  doc.fromHTML(content.innerHTML, 15, 15,{
+    'width':190,
+    'elementHandlers': specialElementHandlers
+  });
+
+  doc.save('test.pdf')
 }
+
+}
+
+
